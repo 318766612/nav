72ff89bd2fed67b8ea2c8423df04932a3d5c2e86
holin <318766612@qq.com>
2024-06-15 20:21:21 +0800

    first commit

---
+++ data/ATool_config.php
@@ -0,0 +1,1 @@
+<?php $config = array( "key" => "abcgklmnoqrtuvwyABHIKLPUVWX45689",  "switch" => 0 );?>
---
+++ data/config.php
@@ -0,0 +1,7 @@
+<?php
+//数据库配置
+$db_config = array(
+    "type" => "sqlite", //类型
+    "file" => "data_1711663040461_fRVZFI6ZH8Yg1M4MySLm.db3" //文件
+);
+?>
